be0577ce855cddb3571b71f97c7fb69e60a8fb2c
Anichek25 <mazinonos@gmail.com>
2016-11-24 12:06:39 -0800

    This is my third memorygame commit

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,11 +3,20 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-	if(cardTwo === cardFour){
+	/*if(cardTwo === cardFour){
 		alert("You found the match!");
 	}
 	else{
 		alert("Sorry try again!");
-	}
+	}*/
 
-	
+	var game-board = document.getElementById("game-board");
+
+	var createBoard = function() {
+	  for(var i = 0; i < cards.lenth; i+=1) {
+		  var cardElement = document.createElement("div");
+		  cardElement.className = "card";
+		  board.appendChild(cardElement);
+  }
+
+}
