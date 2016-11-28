c88bb38666f409d5dcd9242a10b6eb411b020b60
Anichek25 <mazinonos@gmail.com>
2016-11-27 19:52:18 -0800

    This is my final commit of my memorygame

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,12 +3,9 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-	/*if(cardTwo === cardFour){
-		alert("You found the match!");
-	}
-	else{
-		alert("Sorry try again!");
-	}*/
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
 
 	var game-board = document.getElementById("game-board");
 
@@ -16,7 +13,62 @@ var cardFour = "king";
 	  for(var i = 0; i < cards.lenth; i+=1) {
 		  var cardElement = document.createElement("div");
 		  cardElement.className = "card";
-		  board.appendChild(cardElement);
+			cardElement.setAttribute('data-card', cards[i]);
+			cardElement.addEventListener('click', isTwoCards);
+		  gameboard.appendChild(cardElement);
   }
 
 }
+
+var isMatch = function() {
+	if(cardsInPlay[1] == cardsInPlay[3]) {
+		alert("You found the match!");
+	}
+	else{
+		alert("Sorry, try again!");
+	}
+}
+
+var isTwoCards = function() {
+
+	cardsInPlay.push(this.getAttribute('data-card'));
+
+	revealCard(this, cardsInPlay);
+
+  if (cardsInPlay.length === 2) {
+
+		    isMatch(cardsInPlay);
+
+		    cardsInPlay = [];
+
+		  }
+
+		}
+	var refreshCards = function(){
+	   var cardsPlayed = document.querySelectorAll('[data-card]');
+	   for(var i=0; i<cardsPlayed.length; i++){
+		    cardsPlayed[i].innerHTML = " ";
+	}
+
+    cardsInPlay = [];
+}
+
+		//flip the card
+		var revealCard = function(element, cards){
+			if(cards.length === 2){
+				  if(element.getAttribute('data-card')=="queen"){
+					  element.innerHTML = "<img src='queenOakland.jpg', alt='Queen of Oakland'/>";
+				}
+				else{
+					element.innerHTML = "<img src='kingOakland.jpg', alt='King of Oakland'/>";
+				}
+			}
+			else if(element.getAttribute('data-card')=="queen"){
+					element.innerHTML = "<img src='queenHayward.jpg', alt='Queen of Hayward'/>";
+				}
+				else{
+					element.innerHTML = "<img src='kingHayward.jpg', alt='King of Hayward'/>";
+				}
+			}
+		}
+  createBoard();
